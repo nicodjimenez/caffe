49fa7891033adf54fc19430e3e647367d291e7f0
Nicolas D. Jimenez <nicodjimenez@gmail.com>
2014-07-08 19:06:05 -0700

    flipping colors

--- src/caffe/util/io.cpp
+++ src/caffe/util/io.cpp
@@ -85,6 +85,8 @@ bool ReadImageToDatum(const string& filename, const int label,
     LOG(ERROR) << "Could not open or find file " << filename;
     return false;
   }
+  // nico edit: flip image
+  cv_img = cv::Scalar::all(255) - cv_img;
   int num_channels = (iscolor ? 3 : 1);
   datum->set_channels(num_channels);
   datum->set_height(cv_img.rows);
